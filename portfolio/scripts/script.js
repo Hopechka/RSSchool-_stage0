ce1a0bbb94cf91cc0cba24a4bc02fcce85987a5f
Nadezhda Budarina <hopechka@gmail.com>
2022-01-20 16:28:10 +0300

    fix: to match the layout

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -27,7 +27,7 @@ for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
-    const blockID = anchor.getAttribute('href').substr(1);
+    const blockID = anchor.getAttribute('href').slice(1);
 
     document.getElementById(blockID).scrollIntoView({
       behavior: 'smooth',
