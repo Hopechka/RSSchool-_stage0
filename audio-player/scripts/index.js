ee2bb39df3a39b41c06603ee571e21c0b066ddc8
Nadezhda Budarina <hopechka@gmail.com>
2022-02-02 01:59:31 +0300

    feat: set up the progress bar, switching on it

--- audio-player/scripts/index.js
+++ audio-player/scripts/index.js
@@ -9,6 +9,8 @@ const slider = document.querySelector('.slider');
 const singer = document.querySelector('.singer');
 const songName = document.querySelector('.name');
 const albumPicImg = document.querySelector('.album-pic-img');
+const songLength = document.querySelector('.length');
+const current = document.querySelector('.current');
 
 //Название песен
 const songs = ['dontstartnow', 'lemonade'];
@@ -55,7 +57,6 @@ playBtn.addEventListener('click', () => {
 
 //Next song
 function nextSong() {
-  console.log(songIndex);
   songIndex < songs.length - 1 ? songIndex++ : (songIndex = 0);
   loadSong(songs[songIndex]);
   const playIsActive = player.classList.contains('active');
@@ -71,3 +72,39 @@ function prevSong() {
   playIsActive ? PlaySong() : PauseSong();
 }
 prevBtn.addEventListener('click', prevSong);
+
+//Progress bar
+function updateProgress(event) {
+  const { duration, currentTime } = event.srcElement; // duration - длительность, currentTime - текущее время
+  const minDuration = Math.floor((duration / 60) << 0);
+  const secDuration = Math.floor(duration % 60);
+  songLength.innerHTML = `${minDuration}:${secDuration}`;
+  //console.log(duration);
+  const minCurrentTime = Math.floor((currentTime / 60) << 0);
+  const secCurrentTime = Math.floor(currentTime % 60);
+  if (secCurrentTime < 10) {
+    current.innerHTML = `${minCurrentTime}:0${secCurrentTime}`;
+  } else {
+    current.innerHTML = `${minCurrentTime}:${secCurrentTime}`;
+  }
+  //console.log(currentTime);
+  const progressPresent = (currentTime / duration) * 100;
+  myRange.value = progressPresent;
+}
+audio.addEventListener('timeupdate', updateProgress); // 'timeupdate'- св-во аудио
+
+//Set progress
+function setProgress(event) {
+  const width = this.clientWidth;
+  const clickX = event.offsetX;
+  const duration = audio.duration;
+  //console.log(width);
+  //console.log(clickX);
+  //console.log(duration);
+  audio.currentTime = (clickX / width) * duration;
+}
+
+progressContainer.addEventListener('click', setProgress);
+
+//Continue play
+audio.addEventListener('ended', nextSong);
