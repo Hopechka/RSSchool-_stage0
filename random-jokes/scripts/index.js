b780a692f70a7dceaa942af977f85fcce770e28e
Nadezhda Budarina <hopechka@gmail.com>
2022-02-05 01:20:20 +0300

    feat: add style and active pictures

---
+++ random-jokes/scripts/index.js
@@ -0,0 +1,34 @@
+const activeBtn = document.querySelector('.btn');
+//const imgSrc = document.querySelector('.img-colorfully');
+const imgStatic = document.querySelector('.static');
+const imgMove = document.querySelector('.move');
+const jokes = document.querySelector('.jokes');
+
+//Pic change
+function changePicture() {
+  //imgSrc.src = 'images/move.gif';
+  imgMove.classList.toggle('active');
+  imgStatic.classList.toggle('active');
+  activeBtn.classList.toggle('active');
+  body.classList.toggle('active');
+}
+
+activeBtn.addEventListener('click', changePicture);
+
+// Получение данных от API
+const url = 'https://type.fit/api/quotes';
+async function getData() {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data[0].text);
+  showData(data);
+}
+getData();
+
+let currentJoke = 0;
+function showData(data) {
+  currentJoke < data.length - 1 ? currentJoke++ : (currentJoke = 0);
+  jokes.textContent = data[currentJoke].text;
+}
+
+activeBtn.addEventListener('click', getData);
