bdcbbac651dc5b111d8b019d29ced35f4651a16f
Nadezhda Budarina <hopechka@gmail.com>
2022-02-05 10:12:40 +0300

    refractor:add setTimeout for pic

--- random-jokes/scripts/index.js
+++ random-jokes/scripts/index.js
@@ -6,30 +6,34 @@ const jokes = document.querySelector('.jokes');
 
 //Pic change
 function changePicture() {
-  //imgSrc.src = 'images/move.gif';
   imgMove.classList.toggle('active');
   imgStatic.classList.toggle('active');
-  activeBtn.classList.toggle('active');
+  //activeBtn.classList.toggle('active');
   body.classList.toggle('active');
 }
 
 activeBtn.addEventListener('click', changePicture);
+
+activeBtn.addEventListener('click', function () {
+  setTimeout(changePicture, 3000);
+});
 
 // Получение данных от API
 const url = 'https://type.fit/api/quotes';
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data[0].text);
   showData(data);
 }
 getData();
 
 let currentJoke = 0;
+
 function showData(data) {
   currentJoke < data.length - 1 ? currentJoke++ : (currentJoke = 0);
   jokes.textContent = data[currentJoke].text;
   window.localStorage.setItem('joke', currentJoke);
+  console.log(currentJoke + 'currentJoke');
 }
 
 activeBtn.addEventListener('click', getData);
