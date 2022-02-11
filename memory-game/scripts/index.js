f7a7bb427ec9546afcb6603356f9fafbb55f2799
Nadezhda Budarina <hopechka@gmail.com>
2022-02-11 12:25:50 +0300

    feat:logic for game and score

--- memory-game/scripts/index.js
+++ memory-game/scripts/index.js
@@ -1,8 +1,54 @@
 const game = document.querySelector('.game');
 const memoryCards = document.querySelectorAll('.memory-card');
+const score = document.querySelector('.score');
+let count = 0;
 
-function flipCard() {
-  this.classList.toggle('flip');
+let flipCard = false;
+let firstCard, secondCard;
+
+function gameLogic() {
+  this.classList.add('flip');
+
+  if (!flipCard) {
+    flipCard = true;
+    firstCard = this;
+    return;
+    console.log(flipCard);
+  } else {
+    secondCard = this;
+    flipCard = false;
+
+    checkTheSame();
+  }
+
+  console.log('second card');
 }
 
-memoryCards.forEach((card) => card.addEventListener('click', flipCard));
+function checkTheSame() {
+  if (firstCard.dataset.game === secondCard.dataset.game) {
+    stopFlipCard();
+    console.log('the same');
+    return;
+  }
+  comebackFlipCard();
+}
+
+function stopFlipCard() {
+  firstCard.removeEventListener('click', gameLogic);
+  secondCard.removeEventListener('click', gameLogic);
+  count += 10;
+  score.innerHTML = `points: ${count}`;
+}
+
+function comebackFlipCard() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1000);
+  if (count > 0) {
+    count -= 2;
+    score.innerHTML = `points: ${count}`;
+  } else count = 0;
+}
+
+memoryCards.forEach((card) => card.addEventListener('click', gameLogic));
